ae1e0338ccf5432f645398b2e9edb615c0d7b093
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-01-22 13:37:45 +0100

    Start & stop chronometer onclick buttons

--- app/src/main/java/com/ynov/vernet/chronomtre/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/chronomtre/MainActivity.java
@@ -1,14 +1,45 @@
 package com.ynov.vernet.chronomtre;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Chronometer chronometer;
+    Button btnPlay, btnPause;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chronometer = findViewById(R.id.chronometer);
+        btnPlay = findViewById(R.id.btnPlay);
+        btnPause = findViewById(R.id.btnPause);
+
+
+        // Start chronometer
+        btnPlay.setOnClickListener(v -> {
+            chronometer.start();
+
+            // Show button pause
+            btnPause.setVisibility(View.VISIBLE);
+            btnPlay.setVisibility(View.INVISIBLE);
+        });
+
+
+        // Stop chronometer
+        btnPause.setOnClickListener(v -> {
+            chronometer.stop();
+
+            // Show button play
+            btnPlay.setVisibility(View.VISIBLE);
+            btnPause.setVisibility(View.INVISIBLE);
+
+        });
     }
 }
