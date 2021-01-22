bc7a52e8b0b8cadec8e3fe14212ceb58096a1829
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-01-22 15:37:30 +0100

    Add icon & change colors

--- app/src/main/java/com/ynov/vernet/chronomtre/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/chronomtre/MainActivity.java
@@ -2,7 +2,6 @@ package com.ynov.vernet.chronomtre;
 
 import android.os.Bundle;
 import android.os.SystemClock;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
